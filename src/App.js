b4fabcc142c1db207dddc33d846361833cc14f16
Adowalma <adowalmaadao@gmail.com>
2024-12-01 14:44:54 +0100

    #2.1 -  Add .env file to hide  Vercel secrets data

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Contacts, Home, NavBar, Projects, Skills } from './components';
 
 function App() {
+  const projectId = process.env.REACT_APP_VERCEL_PROJECT_ID;
+  const secretKey = process.env.REACT_APP_VERCEL_SECRET_KEY;
+  const url = process.env.REACT_APP_VERCEL_URL;
   return (
     <main>
-      <SpeedInsights/>
+      <SpeedInsights
+        projectId={projectId}
+        secret={secretKey}
+        url={url}
+      />
 
-      <NavBar/>
+      <NavBar />
 
-      <Home/>
+      <Home />
 
-      <Skills/>
+      <Skills />
 
-      <Projects/>
+      <Projects />
 
-      <Contacts/>
-      
+      <Contacts />
+
     </main>
   );
 }
