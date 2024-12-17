f8951c4f8ac738bb57f783b6a8a803e115b4f40c
Adowalma <adowalmaadao@gmail.com>
2024-12-17 21:28:37 +0100

    #12 - Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import { Contacts, Home, NavBar, Projects, Skills } from './components';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
+  const projectId = process.env.REACT_APP_VERCEL_PROJECT_ID;
+  const secretKey = process.env.REACT_APP_VERCEL_SECRET_KEY;
+  const url = process.env.REACT_APP_VERCEL_URL;
   return (
     <main>
+       <SpeedInsights
+        projectId={projectId}
+        secret={secretKey}
+        url={url}
+      />
 
       <NavBar/>
 
